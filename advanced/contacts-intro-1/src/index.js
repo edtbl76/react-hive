77207fa27b438d2c5aa88ec802161cd92cd74ef4
Ed and Gravy <me@emangini.com>
2023-04-08 22:07:57 -0400

    Adding .gitignore and first react project

---
+++ advanced/contacts-intro-1/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { ContactsApp } from './ContactsApp';
+import './index.css';
+
+ReactDOM.render(
+    <ContactsApp />,
+    document.getElementById('root')
+)
